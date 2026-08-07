e4c089093d26213407b2f0ad39136f3f06b5d10d
Luqman <luqman@Luqmans-MacBook-Air.local>
2026-08-07 22:33:11 +0800

    Create Silver staging model

--- bigquery/profiling/duplicates.sql
+++ bigquery/profiling/duplicates.sql
@@ -1,7 +1,22 @@
-SELECT
-    unique_key,
-    COUNT(*) AS duplicate_count
+-- SELECT
+--     unique_key,
+--     COUNT(*) AS duplicate_count
+-- FROM `taxi-data-engineering-504801.bronze.raw_taxi_trips`
+-- GROUP BY unique_key
+-- HAVING COUNT(*) > 1
+-- ORDER BY duplicate_count DESC;
+
+WITH duplicate_keys AS (
+  SELECT unique_key
+  FROM `taxi-data-engineering-504801.bronze.raw_taxi_trips`
+  GROUP BY unique_key
+  HAVING COUNT(*) > 1
+)
+
+SELECT *
 FROM `taxi-data-engineering-504801.bronze.raw_taxi_trips`
-GROUP BY unique_key
-HAVING COUNT(*) > 1
-ORDER BY duplicate_count DESC;
+WHERE unique_key IN (
+  SELECT unique_key
+  FROM duplicate_keys
+)
+ORDER BY unique_key;
