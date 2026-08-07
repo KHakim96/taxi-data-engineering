c4315866b6b111e614c74d1d12a61c095e727c04
Luqman <luqman@Luqmans-MacBook-Air.local>
2026-08-07 21:25:22 +0800

    Add dataset profiling queries

---
+++ bigquery/profiling/profile.sql
@@ -0,0 +1,10 @@
+SELECT
+    COUNT(*) AS total_rows,
+
+    COUNT(DISTINCT unique_key) AS unique_trip_ids,
+
+    MIN(trip_start_timestamp) AS first_trip,
+
+    MAX(trip_start_timestamp) AS last_trip
+
+FROM `taxi-data-engineering-504801.bronze.raw_taxi_trips`;
